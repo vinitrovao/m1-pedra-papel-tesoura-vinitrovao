ca9603182fead133917a5d3bbec73a59a6f7405f
Vinicius Trovão <vinisantostrovao@gmail.com>
2024-10-09 20:39:53 -0300

    Função para o funcionamento do jogo adicionado

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    if (player1 === player2) {
+        return "Empate!";
+    }
+
+    else if (
+        player1 === "Pedra" && player2 === "Tesoura"){
+            return "Jogador 1 venceu!";
+    }
+    else if(
+        player1 === "Papel" && player2 === "Pedra"){
+            return "Jogador 1 venceu!";
+    }
+    else if(
+        player1 === "Tesoura" && player2=== "Papel"){
+            return "Jogador 1 venceu!";
+    }
+    else{
+        return "Jogador 2 venceu!";
+    }
+}
